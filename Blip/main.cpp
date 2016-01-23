0ef5b52227ed2a11bd540fbff9203e5f21799a72
Jeremiah Griffin <jgriffin@xettex.com>
2016-01-23 13:37:54 -0800

    Add project skeleton

---
+++ Blip/main.cpp
@@ -0,0 +1,12 @@
+// Blip
+// Copyright (c) Xettex 2016
+
+/// \file
+/// \brief Contains the \ref main function
+
+/// \brief The entry point of the program
+/// \return exit status of the program
+int main()
+{
+    return 0;
+}
