8a4e8dd5ed5032b8e21dc9833d41bbddf5ee7158
Jeremiah Griffin <jgriffin@xettex.com>
2016-01-28 20:25:19 -0800

    Add updatable interface class

---
+++ Blip/updatable.hpp
@@ -0,0 +1,26 @@
+// Blip
+// Copyright (c) Xettex 2016
+
+/// \file
+/// \brief Contains the \ref blip::updatable class interface
+/// \ingroup virtual
+
+#pragma once
+
+namespace blip {
+
+/// \addtogroup virtual
+/// \{
+
+/// \brief Interface implemented by types that can updated with time
+class updatable
+{
+public:
+    /// \brief Updates the object state by a time step
+    /// \param time_step time step to update by
+    virtual void update(float time_step) = 0;
+};
+
+/// \}
+
+}
